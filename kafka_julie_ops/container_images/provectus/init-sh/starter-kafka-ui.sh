85c42ba7acedbd1a48a997a10b6ed80f46f1ab1c
Cagri <52080028+mcagriaktas@users.noreply.github.com>
2025-06-24 23:30:41 +0300

    Delete kafka_julie_ops directory

--- kafka_julie_ops/container_images/provectus/init-sh/starter-kafka-ui.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -Dspring.config.additional-location=/opt/config.yml -jar /opt/kafka-ui-api-v0.7.2.jar
--- kafka_julie_ops/container_images/team-a/init-sh/machine-starter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Setting up git configuration"
-# Configure git
-git config --global user.name "your_name"
-git config --global user.email "your_email"
-
-echo "Starting machine-starter.sh"
-tail -f /dev/null
